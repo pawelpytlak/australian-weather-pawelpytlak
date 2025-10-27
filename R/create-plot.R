8aeb15d6e2cd42c4261a147e075412ca633569be
pawelpytlak <pawel.pytlak@uk-cpi.com>
2025-10-27 11:52:54 +0000

    sydney to perth

--- R/create-plot.R
+++ R/create-plot.R
@@ -16,7 +16,8 @@ df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
   geom_point(color = "red") + 
-  labs(title = 'Sydney')
+  labs(title = 'Sydney') + 
+  theme_minimal()
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
